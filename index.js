10a42e259f9fddd571bab6578257d0a590841586
Liquidator04 <aritrabag04@gmail.com>
2024-11-29 00:50:01 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const { createServer } = require("node:http");
+const { join } = require("node:path");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+  });
+});
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+io.on("connection", (socket) => {
+  socket.on("hello", (binaryData) => {
+    console.log("Received binary data");
+    // console.log(binaryData);
+    socket.broadcast.emit("hello", binaryData);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("server running at http://localhost:3000");
+});
